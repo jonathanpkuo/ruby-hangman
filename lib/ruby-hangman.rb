2d829fa3fc6acb2e1d33978f232b86cb2d6dc50b
Jonathan Kuo <jonathanpkuo@gmail.com>
2022-03-29 14:34:59 -0700

    Added comments

--- lib/ruby-hangman.rb
+++ lib/ruby-hangman.rb
@@ -1,12 +1,15 @@
 dictionary = Array.new()
 input = File.open('./google-10000-english-no-swears.txt', 'r') do |file|
     while line = file.gets
+        # isolate lines with length > 5 and < 12 
         if (line.chomp.length > 5 && line.chomp.length < 12)
+            # add to dictionary array
             dictionary.push(line.chomp)
         end
     end
 end
 
 def word_picker
+    #picks word at "random" from array
     dictionary[rand(0..dictionary.length)]
 end
