b70b8d8adf82911ae0cc32052da2c67b3e581ccf
Jonathan Kuo <jonathanpkuo@gmail.com>
2022-03-31 12:35:31 -0700

    Split into modules and classes

--- lib/ruby-hangman.rb
+++ lib/ruby-hangman.rb
@@ -1,15 +1,31 @@
-dictionary = Array.new()
-input = File.open('./google-10000-english-no-swears.txt', 'r') do |file|
-    while line = file.gets
-        # isolate lines with length > 5 and < 12 
-        if (line.chomp.length > 5 && line.chomp.length < 12)
-            # add to dictionary array
-            dictionary.push(line.chomp)
+module Hangman
+    
+    class Word_Bank
+        def initialize()
+            @dictionary = Array.new()
+        end
+
+        def print_dictionary
+            puts @dictionary
+        end
+
+        def populate
+            input = File.open('./google-10000-english-no-swears.txt', 'r') do |file|
+                while line = file.gets
+                    # isolate lines with length >= 5 and <= 12
+                    if (line.chomp.length >= 5 && line.chomp.length <= 12)
+                        # add to dictionary array
+                        @dictionary.push(line.chomp)
+                    end
+                end
+            end
+        end
+        
+        def word_picker
+            @dictionary[rand(0..@dictionary.length)]
         end
     end
+
 end
 
-def word_picker(dictionary)
-    #picks word at "random" from array
-    dictionary[rand(0..dictionary.length)]
-end
+game = Hangman::Word_Bank.new()
