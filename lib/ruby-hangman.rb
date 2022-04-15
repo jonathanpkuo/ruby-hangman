a2789f641afe27a0d3436491efad2894fddbc1a2
Jonathan Kuo <jonathanpkuo@gmail.com>
2022-04-14 23:02:22 -0700

    Documentation gets -> getch

--- lib/ruby-hangman.rb
+++ lib/ruby-hangman.rb
@@ -1,30 +1,78 @@
+require 'yaml'
+
 module Hangman
 
     class Hangman_Game
-        attr_accessor :target_word, :progress, :guess_count
+        attr_accessor :target_word, :progress, :chance_count
 
-        def initialize(target_word, guess_count=0, progress=Array.new(target_word.length))
+        def initialize(target_word, chance_count=6, progress=Array.new(target_word.length, "_"), wrong_guesses=Array.new())
             @target_word = target_word
-            @guess_count = guess_count
+            @chance_count = chance_count
             @progress = progress
+            @wordsplit = target_word.chars
             @input_buffer = []
+            @wrong_guesses = wrong_guesses
         end
 
-        def print_status
-            puts "@target_word is #{@target_word} \n @guess_count is #{guess_count} \n @progress is #{progress} \n @input_buffer is #{@input_buffer}."
+        # Debug statement 
+        def print_status 
+            puts "@target_word is #{@target_word} \n @chance_count is #{chance_count} \n @progress is #{progress} \n @input_buffer is #{@input_buffer}."
         end
 
+        # "Standard input function"
         def take_input()
-            @input_buffer.clear() if input_buffer.length > 0
+            @input_buffer.clear() if @input_buffer.length > 0
             @input_buffer.push(STDIN.getch)
         end
         
         def game_play_loop
-            until (@guess_count == 6)
-                
+            # 2 procs for case-switch, using regex /[0-9]/ for nubmers & /[a-z]/ for letters (downcased)
+            number_proc = Proc.new { |x| x.match?(/[0-9]/)}
+            letter_proc = Proc.new { |x| x.downcase.match?(/[a-z]/)}
+            # If-then-elsif for primary gameplay loop
+            # Initial check to see if game is over (lose) gets is to pause
+            if @chance_count == 0
+                puts "Game Over! You Lose!"
+                STDIN.getch
+                return
+            # Secondary check for if game is over (win) gets is to pause
+            elsif @progress.join == @target_word
+                puts "Progress so far: #{@progress.join(" ")}"
+                puts "Game Over! You Win!"
+                STDIN.getch
+                return
+            # If no game over condition is true, allow for guess.
+            else
+                # Basic info statements
+                puts "#{@chance_count} chances remaining."
+                puts "Former guesses: #{@wrong_guesses}"
+                puts "Input Letter or Number (for menu)"
+                puts "Progress so far: #{@progress.join(" ")}"
+                # Input and case/switch
+                take_input()
+                case @input_buffer[0]
+                when number_proc
+                    game_menu()
+                when letter_proc
+                    letter_entry(@input_buffer[0])
+                end
+            end
+            # Call self to loop
+            self.game_play_loop
+        end
+        
+        def letter_entry(letter)
+            if @wordsplit.any?(letter)
+                @wordsplit.each_with_index do |chars, index|
+                    @progress[index] = letter if chars == letter
+                end
+            else
+                @wrong_guesses.push(letter)
+                @chance_count -= 1
             end
         end
 
+        # "In-game" Menu to allow for saving
         def game_menu()
             puts "MENU"
             puts "- 1 - Save Game"
@@ -34,12 +82,13 @@ module Hangman
             take_input()
             case @input_buffer[0]
             when "1"
+                puts "please name your save:"
                 filename = gets.chomp()
                 save_game(filename)
             when "2"
-
+                self.game_play_loop
             when "3"
-
+                return
             when "4"
                 exit
             else 
@@ -50,20 +99,21 @@ module Hangman
 
         def to_yaml
             YAML.dump ({
-                :guess_count => @guess_count,
+                :chance_count => @chance_count,
                 :target_word => @target_word,
-                :progress => @progress
+                :progress => @progress,
+                :wrong_guesses => @wrong_guesses
             })
         end
 
         def save_game(string)
-            filename = "#{string.downcase}.yaml"
+            filename = "saves/#{string.downcase}.yaml"
             File.open(filename, 'w') { |file| file.write(self.to_yaml)}
         end
 
         def self.load_game(file)
             data = YAML.load(File.read(file))
-            self.new(data[:target_word], data[:guess_count], data[:progress])
+            self.new(data[:target_word], data[:chance_count], data[:progress], data[:wrong_guesses])
         end
     end
 
@@ -89,6 +139,7 @@ module Hangman
             when "1"
                 game = Hangman::Hangman_Game.new(@wb.word_picker)
                 game.print_status
+                game.game_play_loop
                 main_menu()
             when "2"
                 load_game_menu()
